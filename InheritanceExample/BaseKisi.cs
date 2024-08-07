bc9a3aa08785eda497ef7a5e19f8d5a832b8ad29
GÖZ'SS <GZ'SS@DESKTOP-NKTDPLS>
2024-08-07 15:08:12 +0300

    Add project files.

---
+++ InheritanceExample/BaseKisi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    // base class 
+    internal abstract class BaseKisi
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Title { get; set; }
+
+        public void ShowBaseKisi()
+        {
+            
+            Console.WriteLine(Title+" Ad Soyad: "+ Ad + " " + Soyad);
+        }
+
+        
+    }
+    // Ogrenci inherite  properties and method of base class
+    internal class Ogrenci : BaseKisi
+    {
+        public int OgrenciNo { get; set; }
+
+        public void ShowOgrenci()
+        {
+            Console.WriteLine("Öğrenci No:"+OgrenciNo );
+            ShowBaseKisi();
+        }
+        
+    }
+
+    // Ogretmen inherite  properties and method of base class
+    internal class Ogretmen : BaseKisi
+    {
+        public decimal Maas { get; set; }
+        public void ShowOgretmen() {
+
+            ShowBaseKisi();
+            Console.WriteLine("Maaş Bilgisi"+ Maas);
+        }
+    }
+}
---
+++ InheritanceExample/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace InheritanceExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci() {Title="Öğrenci", OgrenciNo = 1, Ad="Melike",Soyad="Göz"};
+            Ogretmen ogretmen = new Ogretmen() { Title = "Öğretmen",Ad = "Sertan", Soyad = "Bozkuş", Maas = 70000 };
+
+            ogrenci.ShowOgrenci();
+            ogretmen.ShowOgretmen();
+        }
+    }
+}
